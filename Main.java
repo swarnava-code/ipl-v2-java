1516f5787b155b4e9457b5fc1022c9923aed9a15
swarnavac <swarnava.c.hashcodejr@gmail.com>
2021-11-16 22:36:38 +0530

    Number of matches won of all teams over all the years of IPL

--- Main.java
+++ Main.java
@@ -6,6 +6,11 @@ import java.util.*;//hashmap
 
 
 public class Main {
+	static HashMap<Integer, Integer> noOfMatch = new HashMap<Integer, Integer>();
+	static HashMap<String, Integer> winning = new HashMap<String, Integer>();
+	static HashMap<Integer, HashMap<String, Integer> > win = new HashMap<Integer, HashMap<String, Integer> >();
+
+
 
 /*
 	static Short stringToShort(String string){
@@ -18,11 +23,11 @@ public class Main {
 
 
 	public static void main(String[] args)   {  
-		final int col_path1 = 5; //18
+		//final int col_path1 = 5; //18
 		final String path1 = "csv/matches.csv";//"demo.csv";//archive/matches.csv
-		String next="";
-		int count, key, val;
-		HashMap<Integer, Integer> noOfMatch = new HashMap<Integer, Integer>();
+		String next="", winner="";
+		int count, key, val, year;
+		
 		//Short:Year, Byte:count
 
 		try{
@@ -40,25 +45,63 @@ public class Main {
 
 
 
-					System.out.print(next+":");
+					//System.out.print(next+":");
 					String values[] = next.split(",");
 
+
+					//keys
+					year = Integer.parseInt(values[1]);
+					winner = values[10];
+
+
+					// 2 
+					if(winning.containsKey(winner)){
+						winning.put(winner, winning.get(winner)+1);
+					}else{
+						winning.put(winner, 1);
+					}
+
+
+
+					// 2. Number of matches won of all teams over all the years of IPL.
+					if(win.containsKey(year)){
+						HashMap<String, Integer> inner = win.get(year);
+						if(inner.containsKey(winner)){
+							inner.put(winner, inner.get(winner)+1);
+						}else{
+							inner.put(winner, 1);
+						}
+						win.put(year, inner);
+					} else{
+						HashMap<String, Integer> inner = new HashMap<String, Integer>();
+						inner.put(winner, 1);
+						win.put(year, inner);
+					}
+
+
+
+
+
+					//((Map)win.get(year)).get(team);
+
+
+
+
+
+
+
 					
 
-					key = Integer.parseInt(values[1]);
-
-
 					
-					System.out.print("\n\n:"+key+":\n\n");
+					//System.out.print("\n\n:"+year+":\n\n");
 
 
 					//counting no. of match in each year
-					if(noOfMatch.containsKey(key)){
-						val = noOfMatch.get(key);
-						val++;
-						noOfMatch.put(key, val);
+					// 1. Number of matches played per year of all the years in IPL.
+					if(noOfMatch.containsKey(year)){
+						noOfMatch.put(year, noOfMatch.get(year)+1);
 					}else{
-						noOfMatch.put(key, 1);
+						noOfMatch.put(year, 1);
 					}
 
 
@@ -75,8 +118,14 @@ public class Main {
 		}
 		finally{
 			System.out.println(
-				"Number of matches played per year for all the years in IPL: \n"
+				"1.) Number of matches played per year for all the years in IPL: \n"
 				+noOfMatch);
+
+			winning.remove("");
+			System.out.println(
+				"\n2. Number of matches won of all teams over all the years of IPL. \n"
+				+winning);
+
 		}
 	}  
 }  
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # IPL Project
 ## Play with CSV file using Java
+1. Number of matches played per year of all the years in IPL.
+2. Number of matches won of all teams over all the years of IPL.
+3. For the year 2016 get the extra runs conceded per team.
+4. For the year 2015 get the top economical bowlers.
+5. Create your own scenario.
+
