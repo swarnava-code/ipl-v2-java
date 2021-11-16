1bb7e86db359eccc6e95f178ca508737fd544dc4
swarnavac <swarnava.c.hashcodejr@gmail.com>
2021-11-16 19:14:46 +0530

    count no. of match per year

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
 
 import java.io.*;
 import java.util.Scanner;
+import java.util.*;//hashmap
+
 
 
 public class Main {
 
+/*
+	static Short stringToShort(String string){
+
+	}
+	static Short shortToString(String string){
+
+	}
+	*/
+
 
 	public static void main(String[] args)   {  
 		final int col_path1 = 5; //18
-		final String path1 = "csv/demo.csv";//"demo.csv";//archive/matches.csv
+		final String path1 = "csv/matches.csv";//"demo.csv";//archive/matches.csv
 		String next="";
-		int count;
+		int count, key, val;
+		HashMap<Integer, Integer> noOfMatch = new HashMap<Integer, Integer>();
+		//Short:Year, Byte:count
 
 		try{
 				Scanner sc = new Scanner(new File(path1));
@@ -18,6 +31,7 @@ public class Main {
 				sc.useDelimiter("\n");   //sets the delimiter pattern  
 				//count = 1;
 
+				sc.next();//to eliminate heading, otherwise NumberFormatException happen
 				while (sc.hasNext()) {
 
 
@@ -27,28 +41,42 @@ public class Main {
 
 
 					System.out.print(next+":");
-
 					String values[] = next.split(",");
 
-					System.out.print("\n\n:"+values[1]+":\n\n");
-					//System.out.println();
+					
 
-/*
-					if(count>col_path1-1){
-						count = 0;
-						System.out.print("Hello");
+					key = Integer.parseInt(values[1]);
+
+
+					
+					System.out.print("\n\n:"+key+":\n\n");
+
+
+					//counting no. of match in each year
+					if(noOfMatch.containsKey(key)){
+						val = noOfMatch.get(key);
+						val++;
+						noOfMatch.put(key, val);
+					}else{
+						noOfMatch.put(key, 1);
 					}
-					if(next.equals("\n"))
-						System.out.println("SWARNAVA CHAKRABORTY");
-						*/
 
-					//count++;
+
+				
 				}   
 				sc.close();
 
 
 		}catch(FileNotFoundException e){
 			System.out.println("File not found : "+path1+"\nDetails: "+e);
+		}
+		catch(NumberFormatException e){
+			System.out.println("number format exception : "+"\nDetails: "+e);
+		}
+		finally{
+			System.out.println(
+				"Number of matches played per year for all the years in IPL: \n"
+				+noOfMatch);
 		}
 	}  
 }  
