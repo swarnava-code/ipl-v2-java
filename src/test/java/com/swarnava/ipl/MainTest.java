e97dae8d3e270b6b34e9eb4a9269b712ad4f54ad
swarnava.18 <swarnava.18@mountblue.tech>
2022-01-10 16:43:03 +0530

    all methods called from MainTest to get actual value and testmethods are created for compare

--- src/test/java/com/swarnava/ipl/MainTest.java
+++ src/test/java/com/swarnava/ipl/MainTest.java
@@ -1,25 +1,58 @@
 package com.swarnava.ipl;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.util.*;
+
+import static com.swarnava.ipl.Main.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
+    static Main main = new Main();
+    static Map<String, Float> actualTopEconomicalBowlersForParticularYear;
+    static Map<Integer, Integer> actualNumberOfMatchesPlayedPerYear;
+    static Map<String, Integer> actualNumberOfMatchesWonOfAllTeam;
+    static Map<String, Integer> actualExtraRunsConcededPerTeamForParticularYear;
+    static TreeSet<String> actualWinnersWhoWinInACityLeastOneTime;
+    static Map<String, Float> expectedTopEconomicalBowlersForParticularYear;
+    static Map<Integer, Integer> expectedNumberOfMatchesPlayedPerYear;
+    static Map<String, Integer> expectedNumberOfMatchesWonOfAllTeam;
+    static Map<String, Integer> expectedExtraRunsConcededPerTeamForParticularYear;
+    static List<String> expectedWinnersWhoWinInACityLeastOneTime;
 
-    @BeforeEach
-    void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+    @BeforeAll
+    static void init() {
+        if (main.setConnection()) {
+            actualNumberOfMatchesPlayedPerYear = printNumberOfMatchesPlayedPerYear();
+            actualNumberOfMatchesWonOfAllTeam = printNumberOfMatchesWonOfAllTeam();
+            actualExtraRunsConcededPerTeamForParticularYear = printTheExtraRunsConcededPerTeamForParticularYear(2016);
+            actualTopEconomicalBowlersForParticularYear = printTheTopEconomicalBowlersForParticularYear(2015);
+            actualWinnersWhoWinInACityLeastOneTime = new TreeSet<>(printTheWinnersWhoWinInACityLeastOneTime("Kolkata"));
+        }
     }
 
     @Test
-    void main() {
+    void testPrintNumberOfMatchesPlayedPerYear(){
+        fail("not implemented");
     }
 
     @Test
-    void setConnection() {
+    void testPrintNumberOfMatchesWonOfAllTeam(){
+        fail("not implemented");
+    }
+
+    @Test
+    void testPrintTheExtraRunsConcededPerTeamForParticularYear(){
+        fail("not implemented");
+    }
+
+    @Test
+    void testPrintTheTopEconomicalBowlersForParticularYear(){
+        fail("not implemented");
+    }
+
+    @Test
+    void testPrintTheWinnersWhoWinInACityLeastOneTime(){
+        fail("not implemented");
     }
 }
